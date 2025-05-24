96cec45c12984ee652b18a753197a195d143036a
Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>
2025-05-24 17:05:34 +0100

    Updated midNav

--- src/navigation/MidNav.tsx
+++ src/navigation/MidNav.tsx
@@ -13,55 +13,75 @@ import {
 } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 import Image from "next/image";
-// import { debounce } from "lodash";
-
 
 export const MidNav: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
+
   return (
-    <Flex bg="white" py={3} px={{ base: 4, xl: 5 }} display={{ base: "flex", lg: "flex" }}>
+    <Flex
+      bg="white"
+      py={3}
+      px={{ base: 4, xl: 5 }}
+      direction={{ base: "column", md: "row" }}
+      align={{ base: "stretch", md: "center" }}
+      justify="space-between"
+      gap={{ base: 3, md: 0 }}
+    >
       {/* Logo Section */}
-      <Box width="33.33%" className="d-md-block d-none">
-        <Link href="/" textDecoration="none">
-          <Image 
-            src="/images/logo/trans_logo.png" 
-            width={40} 
+      <Box
+        width={{ base: "100%", md: "33.33%" }}
+        mb={{ base: 2, md: 0 }}
+        textAlign={{ base: "center", md: "left" }}
+        display={{ base: "block", md: "block" }}
+      >
+        <Link href="/" textDecoration="none" display="inline-block">
+          <Image
+            src="/images/logo/trans_logo.png"
+            width={40}
             height={40}
-            alt="sitelogo" />
+            alt="sitelogo"
+          />
         </Link>
       </Box>
 
       {/* Search Bar */}
-      <Box width="33.33%" className="shift" textAlign="left">
+      <Box
+        width={{ base: "100%", md: "33.33%" }}
+        textAlign="center"
+      >
         <FormControl>
-          <InputGroup>
+          <InputGroup maxW={{ base: "100%", md: "300px" }} mx="auto">
             <Input
               placeholder="Search for products"
               bg="white"
               borderColor="gray.300"
-              className="mt-1"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-            //   onKeyPress={(e) => {
-            //     if (e.key === "Enter") // handleSearch();
-            //   }}
+              size="md"
             />
             <InputRightElement
               bg="transparent"
               color="primary.500"
-            //   onClick={handleSearch}
               cursor="pointer"
+              pointerEvents="auto"
+              _hover={{ color: "primary.700" }}
             >
-              <Icon as={FaSearch} className="mt-2" />
+              <Icon as={FaSearch} />
             </InputRightElement>
           </InputGroup>
         </FormControl>
       </Box>
 
       {/* Customer Service Info */}
-      <Box width="33.33%" textAlign="right">
-        <Text m={0} fontWeight="bold" color="#C28840">Contact</Text>
-        <Heading as="h5" size="xs" m={0}>
+      <Box
+        width={{ base: "100%", md: "33.33%" }}
+        mt={{ base: 3, md: 0 }}
+        textAlign={{ base: "center", md: "right" }}
+      >
+        <Text fontWeight="bold" color="#C28840" mb={1}>
+          Contact
+        </Text>
+        <Heading as="h5" size="xs" m={0} mb={1}>
           +234 703 473 9950
         </Heading>
         <Heading as="h5" size="xs" m={0}>
