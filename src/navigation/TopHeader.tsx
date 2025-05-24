3d55ee84cc457320b6de6a17540e2ca3278c5fc1
Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>
2025-05-24 17:09:47 +0100

    Updated topNav

--- src/navigation/TopHeader.tsx
+++ src/navigation/TopHeader.tsx
@@ -1,62 +1,98 @@
 import React from 'react';
-import { Box, Flex, Link, Button, Menu, MenuButton, MenuList, MenuItem, Badge } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Badge,
+  Icon,
+} from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { FaHeart, FaShoppingCart } from 'react-icons/fa';
 
 export const TopHeader: React.FC = () => {
   return (
-    <Box className="container-fluid">
-      <Flex className="row py-1 px-xl-5" style={{ backgroundColor: "#000"}}>
+    <Box bg="black" px={{ base: 4, xl: 10 }} py={2}>
+      <Flex
+        direction={{ base: "column", lg: "row" }}
+        align={{ base: "center", lg: "center" }}
+        justify="space-between"
+      >
         {/* Left-side links */}
-        <Box className="col-lg-6 d-none d-lg-block">
-          <Flex align="center" h="100%">
-            <Link color="gray.100" mr={3} href="/">Home</Link>
-            <Link color="gray.100" mr={3} href="/about">About</Link>
-            <Link color="gray.100" mr={3} href="/faqs">FAQs</Link>
-            <Link color="gray.100" mr={3} href="/blog">Blog</Link>
-            <Link color="gray.100" mr={3} href="/contact">Contact</Link>
+        <Flex
+          display={{ base: "none", lg: "flex" }}
+          align="center"
+          gap={4}
+        >
+          <Link color="gray.100" href="/">Home</Link>
+          <Link color="gray.100" href="/about">About</Link>
+          <Link color="gray.100" href="/faqs">FAQs</Link>
+          <Link color="gray.100" href="/blog">Blog</Link>
+          <Link color="gray.100" href="/contact">Contact</Link>
+        </Flex>
+
+        {/* Right-side: My Account Dropdown & Icons */}
+        <Flex
+          align="center"
+          justify={{ base: "center", lg: "flex-end" }}
+          gap={4}
+          mt={{ base: 2, lg: 0 }}
+          width="100%"
+        >
+          {/* My Account Dropdown */}
+          <Menu>
+            <MenuButton
+              as={Button}
+              size="sm"
+              bg="gray.100"
+              color="black"
+              _hover={{ bg: "gray.200" }}
+              rightIcon={<ChevronDownIcon />}
+            >
+              My Account
+            </MenuButton>
+            <MenuList>
+              <MenuItem as="a" href="/login">Login</MenuItem>
+              <MenuItem as="a" href="/register">Register</MenuItem>
+            </MenuList>
+          </Menu>
+
+          {/* Icons (visible on mobile only) */}
+          <Flex display={{ base: "flex", lg: "none" }} align="center" gap={4}>
+            <Box position="relative">
+              <Icon as={FaHeart} fontSize="20px" color="gray.200" />
+              <Badge
+                position="absolute"
+                top="-1"
+                right="-2"
+                fontSize="0.7em"
+                colorScheme="red"
+                borderRadius="full"
+              >
+                0
+              </Badge>
+            </Box>
+
+            <Box position="relative">
+              <Icon as={FaShoppingCart} fontSize="20px" color="gray.200" />
+              <Badge
+                position="absolute"
+                top="-1"
+                right="-2"
+                fontSize="0.7em"
+                colorScheme="green"
+                borderRadius="full"
+              >
+                0
+              </Badge>
+            </Box>
           </Flex>
-        </Box>
-
-        {/* Right-side menu and buttons */}
-        <Box className="col-lg-6 text-center text-lg-right">
-          <Flex align="center" justify={{ base: "center", lg: "flex-end" }}>
-            {/* My Account Dropdown */}
-            <Menu>
-              <MenuButton as={Button} size="sm" bg="gray.100" rightIcon={<ChevronDownIcon />}>
-                My Account
-              </MenuButton>
-              <MenuList>
-                <MenuItem><a href="/login">Login</a></MenuItem>
-                <MenuItem><a href="/register">Register</a></MenuItem>
-              </MenuList>
-            </Menu>
-
-            {/* Currency Dropdown */}
-            {/* <Menu>
-              <MenuButton as={Button} size="sm" bg="gray.100" mx={2} rightIcon={<ChevronDownIcon />}>
-                USD
-              </MenuButton>
-              <MenuList>
-                <MenuItem>EUR</MenuItem>
-                <MenuItem>GBP</MenuItem>
-                <MenuItem>CAD</MenuItem>
-              </MenuList>
-            </Menu>            */}
-          </Flex>
-
-          {/* Heart and Cart Icons for Mobile */}
-          <Flex display={{ base: "flex", lg: "none" }} align="center" ml={2}>
-            {/* Favorites (Heart) Icon */}
-            <FaHeart color="gray.600" fontSize="24px" />
-            <Badge color="gray.600" border="1px solid" borderRadius="full" ml={-2} mb={1}>0</Badge>
-
-            {/* Cart Icon */}
-            <FaShoppingCart color="gray.600" fontSize="24px" style={{ marginLeft: '16px' }} />
-            <Badge color="gray.600" border="1px solid" borderRadius="full" ml={-2} mb={1}>0</Badge>
-          </Flex>
-        </Box>
+        </Flex>
       </Flex>
     </Box>
   );
-}
+};
