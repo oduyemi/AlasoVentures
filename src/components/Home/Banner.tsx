aed3e859fcdb003ae8b1453cb00e38b8af561a0e
Yemi Oduyemi <yhermii@gmail.com>
2026-03-19 16:45:30 +0100

    updated

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -94,7 +94,7 @@ export const Banner: React.FC = () => {
               >
                 <MotionImage
                   src={item.imgSrc}
-                  alt={item.title}
+                  alt="heroImage"
                   width="100%"
                   height="100%"
                   loading="lazy"
